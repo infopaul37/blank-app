2e6fc27f22624e54c004db1b004cc1e7d424bd85
AugusteP10 <petit.auguste7@gmail.com>
2024-09-04 10:00:04 +0000

    j'ai fait un tableau

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,19 @@ st.title("Weather App ")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+import streamlit as st
+import pandas as pd
+
+df = pd.DataFrame(
+    [
+        {"temperature": "st.selectbox", "place": "paris"},
+        {"temperature": "st.balloons", "place": "tokyo"},
+        {"temperature": "st.time_input", "place": "Londres"}
+    ]
+)
+
+st.dataframe(df, use_container_width=True)
+lien_api = "https://api.weatherbit.io/v2.0/current" 
++"&city=Paris&country=France"
++ "&key=32af0f6ca0b24b1793376130cc6b9b9b" 
++ "&include=minutely"
